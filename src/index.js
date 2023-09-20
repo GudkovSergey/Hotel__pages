fcf85788e062182769ea40b41f26ce127315d8e2
GudkovSergey <gudkow-sergey@mail.ru>
2023-09-20 20:59:41 +0500

    Created and connected icon fonts based on svg from figma

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './pages/colorsType/colorsType';
 import './pages/formElements/formElements';
 import './components/logo/logo';
-import './components/textField/textField'
-import './components/dropdown__guest/dropdown_guest'
+import './components/textField/textField';
+import './components/dropdown__guest/dropdown_guest';
+
 
