5a434e52fb3d98fcce0521dccdc6bf85d50f929b
GudkovSergey <gudkow-sergey@mail.ru>
2023-10-13 14:23:01 +0500

    made an input field mask using a jquery- maskp-lugin

---
+++ src/components/maskedField/maskedField.js
@@ -0,0 +1,6 @@
+import './maskedField.scss';
+import './jquery.maskedinput.min';
+$(document).ready(function () {
+    $("#date").mask("99.99.9999");
+
+});
--- src/pages/formElements/formElements.js
+++ src/pages/formElements/formElements.js
@@ -1,1 +1,6 @@
-import './formElements.scss'
+import './formElements.scss';
+import '../../components/logo/logo';
+import '../../components/textField/textField';
+import '../../components/dropdown__guest/dropdown_guest';
+import '../../components/textField_hover/textField_hover';
+import '../../components/maskedField/maskedField';
