aac29bb0024b7259863aa1e122f580bd3f673d48
GudkovSergey <gudkow-sergey@mail.ru>
2023-11-01 09:42:58 +0300

    Refactored dataPicker . Created a configuration file with a connection in any component with the ability to add options.

--- src/components/dateDropdown/dateDropdown.js
+++ src/components/dateDropdown/dateDropdown.js
@@ -1,5 +1,7 @@
 import './dateDropdown.scss';
 import 'air-datepicker/air-datepicker.css';
-import '../datapickerPluginSetting/datapicker'
+import AirDatepicker from 'air-datepicker';
+import {datapickerConfig} from '../datapickerPluginSetting/datapicker'
 
 
+new AirDatepicker('.datepicker',datapickerConfig());
--- src/components/dateDropdownToday/dateDropdown__today.js
+++ src/components/dateDropdownToday/dateDropdown__today.js
@@ -1,32 +1,7 @@
 import './dateDropdown__today.scss';
-
-
+import {datapickerConfig} from '../datapickerPluginSetting/datapicker'
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 
-
-
-new AirDatepicker('.datapicker__today', {
-    buttons: ['clear',
-        {
-            content(dp){
-                return 'Применить'
-            }
-        }],
-    selectedDates: [new Date()],
-    prevHtml: '<svg class="arrow__prev" width="17" height="18" viewBox="0 0 17 18"  xmlns="http://www.w3.org/2000/svg">\n' +
-        '<path  d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/>\n' +
-        '</svg>',
-    nextHtml:'<svg class=" arrow__next" width="17" height="18" viewBox="0 0 17 18"  xmlns="http://www.w3.org/2000/svg">\n' +
-        '<path  d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" />\n' +
-        '</svg>\n',
-    navTitles: {
-        days: 'MMMM <i>yyyy</i>',
-        months: 'yyyy',
-        years: 'yyyy1 - yyyy2'
-    }
-
-
-
-
-})
+const selectedDates = [new Date()];
+new AirDatepicker('.datapicker__today',datapickerConfig(selectedDates));
