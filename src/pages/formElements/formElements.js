f06b067433692fb8cadc46ee46bf8230a17f5478
GudkovSergey <gudkow-sergey@mail.ru>
2023-11-22 14:31:18 +0500

    test

--- src/pages/formElements/formElements.js
+++ src/pages/formElements/formElements.js
@@ -4,7 +4,7 @@ import '../../components/textField/textField';
 import '../../components/dropdown-with-button/dropdownWithButton';
 import '../../components/textField_hover/textField_hover';
 import '../../components/maskedField/maskedField';
-import  '../../components/dateDropdown/dateDropdown';
+import '../../components/dateDropdown/dateDropdown';
 import '../../components/dateDropdownToday/dateDropdown__today';
 import '../../components/filterDateDropdown/filterDateDropdown';
 import '../../components/subscribeTextField/subscribeTextField';
